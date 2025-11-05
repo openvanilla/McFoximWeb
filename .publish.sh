08c93d2a526d27ef2d454099c6740f6acb0133f7
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2025-11-06 01:26:26 +0800

    Update PIME configuration and build files

--- .publish.sh
+++ .publish.sh
@@ -1,1 +1,3 @@
+npm install
+npm run build
 firebase deploy --only hosting
---
+++ build_pime.bat
@@ -0,0 +1,15 @@
+@REM Run as administrator
+
+echo * Build McBopomopfo for PIME
+call npm run build:pime
+echo * Delete old files
+rmdir /S /Q "C:\Program Files (x86)\PIME\node\input_methods\mcbopomofo"
+echo * Copy new files
+xcopy /E /I ".\output\pime" "C:\Program Files (x86)\PIME\node\input_methods\mcbopomofo"
+
+@REM echo * Register McFoxIM
+@REM "C:\Program Files (x86)\PIME\node\input_methods\mcbopomofo\run_register_ime.bat"
+
+
+echo "Please restart PIME Launcher to see the changes."
+
